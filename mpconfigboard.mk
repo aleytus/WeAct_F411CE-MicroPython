ad57ae3130078e049bfd4c33bb34a95a319fa287
Vladislav <steam.8ladi7la8@yandex.ru>
2021-06-29 20:36:29 +0700

    add USSL(UHASHLIB, UCRYPTOLIB) and MBEDTLS(UBINASCII)

--- mpconfigboard.mk
+++ mpconfigboard.mk
@@ -1,9 +1,11 @@
 MCU_SERIES = f4
 CMSIS_MCU = STM32F411xE
 AF_FILE = boards/stm32f411_af.csv
-LD_FILES = boards/stm32f411.ld boards/common_ifs.ld
+LD_FILES = boards/WeAct_F411CE/stm32f411.ld boards/common_ifs.ld
 TEXT0_ADDR = 0x08000000
-TEXT1_ADDR = 0x08020000
+TEXT1_ADDR = 0x0800c000
 
 # MicroPython settings
 MICROPY_VFS_LFS2 = 1
+MICROPY_PY_USSL = 1
+MICROPY_SSL_MBEDTLS = 1
